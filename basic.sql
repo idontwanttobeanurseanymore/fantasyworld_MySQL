c0708f8a953279d20800a2659bdc470cfbb14679
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-06-09 18:42:11 +0200

    more tables

--- basic.sql
+++ basic.sql
@@ -12,3 +12,20 @@ CREATE TABLE magician(
 name VARCHAR(45) NOT NULL,
 speciality VARCHAR(64) NOT NULL,
 mastery ENUM('apprentice', 'master', 'archmage') NOT NULL);
+CREATE TABLE schools(
+name VARCHAR(45) NOT NULL,
+location VARCHAR(64) NOT NULL,
+description VARCHAR(1024));
+
+CREATE TABLE magic_wand(
+wood VARCHAR(45) NOT NULL,
+core VARCHAR(64) NOT NULL,
+length VARCHAR(45) NOT NULL,
+flexibility VARCHAR(45) NOT NULL);
+
+CREATE TABLE mythical_creatures(
+name VARCHAR(45) NOT NULL,
+type VARCHAR(45) NOT NULL,
+skill VARCHAR(64) NOT NULL,
+size ENUM('small', 'medium', 'big') NOT NULL,
+diet ENUM('herbivore', 'carnivore', 'omnivore') NOT NULL);
